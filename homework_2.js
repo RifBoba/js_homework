b653d62bfc1a81c4e4699ebcc739b538e7aa1321
Dmytro <demetribinas@gmail.com>
2024-04-14 22:48:43 +0200

    homework_8

--- homework_2.js
+++ homework_2.js
@@ -1,56 +1,31 @@
-// Створюємо об'єкт з послугами та їх вартістю
-const services = {
-    "кебаб": "400 грн",
-    "люля кебаб": "280 грн",
-    "шаурма": "340 грн"
-  };
-  
-  // Додаємо нову послугу та її вартість
-  services['Кетчуп'] = "25 грн";
-  services['Майонез'] = "30 грн";
-  
-  // Метод, який обчислює загальну вартість послуг
-  services.price = function() {
-    let total = 0;
-    for (const service in this) {
-      if (typeof this[service] === 'string') {
-        // Видаляємо " грн" зі строки та перетворюємо на число
-        const price = parseFloat(this[service].replace(' грн', ''));
-        total += price;
-      }
-    }
-    return total + ' грн';
-  };
-  
-  // Метод, який повертає мінімальну вартість послуг
-  services.minPrice = function() {
-    let min = Infinity;
-    for (const service in this) {
-      if (typeof this[service] === 'string') {
-        const price = parseFloat(this[service].replace(' грн', ''));
-        if (price < min) {
-          min = price;
-        }
-      }
-    }
-    return min + ' грн';
-  };
-  
-  // Метод, який повертає максимальну вартість послуг
-  services.maxPrice = function() {
-    let max = 0;
-    for (const service in this) {
-      if (typeof this[service] === 'string') {
-        const price = parseFloat(this[service].replace(' грн', ''));
-        if (price > max) {
-          max = price;
-        }
-      }
-    }
-    return max + ' грн';
-  };
-  
-  // Виводимо результати
-  console.log('Загальна вартість послуг:', services.price());
-  console.log('Мінімальна вартість послуг:', services.minPrice());
-  console.log('Максимальна вартість послуг:', services.maxPrice());
+// Масив об'єктів з email-адресами
+var arr = [
+  {
+      userName: "Ayleen",
+      lastName: "Mamamilk",
+      email: "ayleen_mamamilk@gmail.com"
+  },
+  {
+      userName: "Dmytro",
+      lastName: "Panatsey",
+      email: "Dmytro.Panatsey@yahoo.com"
+  },
+  {
+      userName: "stariy",
+      lastName: "did",
+      email: "stariy_did@ukr.net"
+  }
+];
+
+// Регулярний вираз для перевірки email-адрес
+var validEmailRegex = /^[a-zA-Z0-9.-_]+@(gmail\.com|yahoo\.com)$/;
+
+// Фільтруємо адреси, які підходять за умовами
+var trustedEmails = arr.filter(function(obj) {
+  return validEmailRegex.test(obj.email);
+});
+
+// Виводимо результат
+for (var i = 0; i < trustedEmails.length; i++) {
+  console.log("Email-адреса:", trustedEmails[i].email);
+}
